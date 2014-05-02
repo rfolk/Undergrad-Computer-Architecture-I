8efcb8bb7a271a04581f0330372596257db25c19
Russell Folk <russellfolk@gmail.com>
2014-05-02 16:06:52 -0600

    cache aware trial

--- Homework 5/cachelab-handout/trans.c
+++ Homework 5/cachelab-handout/trans.c
@@ -16,7 +16,10 @@
 #include <stdio.h>
 #include "cachelab.h"
 
+ #define OBLIV_MIN	4	// number of words in submatrix when cache oblivious gives up
+
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+void recursive_transpose( int M, int N, int row_offset, int col_offset, int A[N][M], int B[M][N] );
 
 /*
  * transpose_submit - This is the solution transpose function that you
@@ -28,45 +31,43 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-	int i, j, k, l;
-	int tmp;
-	int blocksize1;
-	int blocksize2;
-	if ( M == 32 )
-	{
-		blocksize1 = 4;
-		blocksize2 = 4;
-	}
-	else if ( M == 64 )
-	{
-		blocksize1 = 8;
-		blocksize2 = 8;
-	}
-	else
-	{
-		blocksize1 = 1;
-		blocksize2 = 1;
-	}
-	for ( i = 0; i < N; i += blocksize1 )
-	{
-		for ( j = 0; j < M; j += blocksize2 )
-		{
-			for ( k = i; k < i + blocksize1; ++k )
-			{
-				for ( l = j; l < j + blocksize2; ++l )
-				{
-					tmp = A[ k ][ l ];
-					B[ l ][ k ] = tmp;
-				}
-			}
-		}
-	}
+	recursive_transpose( M, N, 0, 0, A, B );
 }
 
 /*
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started.
  */
+
+void recursive_transpose( int M, int N, int row_offset, int col_offset, int A[N][M], int B[M][N] )
+{
+	if ( M > OBLIV_MIN || N > OBLIV_MIN )
+	{
+		if ( N >= M )
+		{
+			int n_halfed = N / 2;
+			recursive_transpose( M, n_halfed, row_offset, col_offset, A, B );
+			recursive_transpose( M, n_halfed, row_offset, col_offset + n_halfed, A, B );
+		}
+		else
+		{
+			int half_m = M / 2;
+			recursive_transpose( half_m, N, row_offset, col_offset, A, B );
+			recursive_transpose( half_m, N, row_offset + half_m, col_offset, A, B );
+		}
+
+	}
+	else
+	{
+		int rlimit = row_off + N;
+		int climit = col_off + M;
+		int i, j
+		for ( i = row_off; i < rlimit; ++i )
+			for( j = col_off; j < climit; ++j )
+				A[j][i] = B[i][j];
+	}
+}
+
 
 /*
  * trans - A simple baseline transpose function, not optimized for the cache.
