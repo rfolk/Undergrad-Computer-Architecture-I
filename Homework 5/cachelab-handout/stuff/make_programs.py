1d236decc156b02d8a3c65fd9c2f3777bdbd9b82
Russell Folk <russellfolk@gmail.com>
2014-05-02 18:39:52 -0600

    let's automate...

---
+++ Homework 5/cachelab-handout/stuff/make_programs.py
@@ -0,0 +1,49 @@
+#!/opt/local/bin/python
+
+for i in range(1,65):
+    for j in range(1,65):
+        filename = "trans_" + str(i) + "_" + str(j) + ".c"
+        f = open(filename,"w")
+        f.write("#include <stdio.h>\n#include \"cachelab.h\"\n")
+        f.write("int is_transpose(int M, int N, int A[N][M], int B[M][N]);\n")
+        f.write("void transpose_submit_{0}_{1}(int M, int N, int A[N][M], int B[M][N]);\n".format(i, j))
+
+        f.write("\n\n\n\n\n")
+
+        f.write("char transpose_submit_desc[] = \"Transpose submission\";\n")
+        f.write("void transpose_submit(int M, int N, int A[N][M], int B[M][N])\n")
+        f.write("{\n")
+        f.write(" int i, j, k, l;\n int tmp;\n    int block1;\n    int block2;\n")
+        f.write(" if ( M == 32 )\n{\n")
+        f.write("     block1 = {0};\n        block2 = {1};\n".format(i, j))
+        f.write("  }\n")
+        f.write(" else if ( M == 64 )\n    {\n")
+        f.write("     block1 = {0};\n        block2 = {1};\n".format(i, j))
+        f.write("    }\n")
+        f.write(" else\n    {\n        block1 = 1;\n        block2 = 1;\n    }\n")
+        f.write(" for ( i = 0; i < N; i += block1 )\n    {\n        for ( j = 0; j < M; j += block2 )\n        {\n            for ( k = i; k < i + block1; ++k )\n            {\n                for ( l = j; l < j + block2; ++l )\n                {\n")
+        f.write("                    tmp = A[ k ][ l ];\n                    B[ l ][ k ] = tmp;\n")
+        f.write("                }\n            }\n        }\n    }\n")
+        f.write("}\n")
+
+        f.write("\n\n\n\n\n")
+
+        f.write("void registerFunctions()\n")
+        f.write("{\n")
+        f.write("    registerTransFunction(transpose_submit, transpose_submit_desc);\n".format(i, j))
+        f.write("}\n")
+
+        f.write("int is_transpose(int M, int N, int A[N][M], int B[M][N])\n")
+        f.write("{\n")
+        f.write("    int i, j;\n")
+        f.write("    for (i = 0; i < N; i++) {\n")
+        f.write("        for (j = 0; j < M; ++j) {\n")
+        f.write("            if (A[i][j] != B[j][i]) {\n")
+        f.write("                return 0;\n")
+        f.write("            }\n")
+        f.write("        }\n")
+        f.write("    }\n")
+        f.write("    return 1;\n")
+        f.write("}\n")
+        f.close()
+
---
+++ Homework 5/cachelab-handout/stuff/trans_58_38.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include "cachelab.h"
+int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+void transpose_submit_58_38(int M, int N, int A[N][M], int B[M][N]);
+
+
+
+
+
+char transpose_submit_desc[] = "Transpose submission";
+void transpose_submit(int M, int N, int A[N][M], int B[M][N])
+{
+ int i, j, k, l;
+ int tmp;
+    int block1;
+    int block2;
+ if ( M == 32 )
+{
+     block1 = 58;
+        block2 = 38;
+  }
+ else if ( M == 64 )
+    {
+     block1 = 58;
+        block2 = 38;
+    }
+ else
+    {
+        block1 = 1;
+        block2 = 1;
+    }
+ for ( i = 0; i < N; i += block1 )
+    {
+        for ( j = 0; j < M; j += block2 )
+        {
+            for ( k = i; k < i + block1; ++k )
+            {
+                for ( l = j; l < j + block2; ++l )
+                {
+                    tmp = A[ k ][ l ];
+                    B[ l ][ k ] = tmp;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+void registerFunctions()
+{
+    registerTransFunction(transpose_submit, transpose_submit_desc);
+}
+int is_transpose(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; ++j) {
+            if (A[i][j] != B[j][i]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
