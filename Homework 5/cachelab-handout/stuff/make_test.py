5dc4a33425be7e6a3bdd7b924a6bbd248ca43ff2
Russell Folk <russellfolk@gmail.com>
2014-05-02 21:13:40 -0600

    make sure the results print to file...

--- Homework 5/cachelab-handout/stuff/make_test.py
+++ Homework 5/cachelab-handout/stuff/make_test.py
@@ -7,8 +7,8 @@ for i in range(1,65):
         build_dir = "test_" + str(i) + "_" + str(j)
         try:
         		os.chdir(build_dir)
-        		os.system("./test-trans -M 32 -N 32 | grep TEST_TRANS_RESULTS")
-        		os.system("./test-trans -M 64 -N 64 | grep TEST_TRANS_RESULTS")
-        		os.system("./test-trans -M 61 -N 67 | grep TEST_TRANS_RESULTS")
+        		os.system("./test-trans -M 32 -N 32 | grep TEST_TRANS_RESULTS > 32x32_results")
+        		os.system("./test-trans -M 64 -N 64 | grep TEST_TRANS_RESULTS > 64x64_results")
+        		os.system("./test-trans -M 61 -N 67 | grep TEST_TRANS_RESULTS > 61x67_results")
         finally:
         		os.chdir(cwd)
